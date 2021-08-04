d94b759e28854faa273e4efe66f0c3a2e9d60aa6
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-08-04 03:21:13 +0200

    added two

---
+++ config/database.js
@@ -0,0 +1,18 @@
+
+
+// Use this configuration for an SQLite database on your machine.
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
